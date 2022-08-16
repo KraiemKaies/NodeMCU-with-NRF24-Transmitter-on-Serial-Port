32cb98f300b260298ee6c2bb63a4b762e56b1b59
Kaies Kraiem <kraiem.kaies@gmail.com>
2022-08-16 10:57:33 +0100

    Initial commit

---
+++ NodeMCU with NRF24 Transmitter on Serial Port.ino
@@ -0,0 +1,33 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(2, 4);                // CE, CSN
+const byte address[6] = "00001";  //Byte of array representing the address. This is the address where we will send the data. This should be same on the receiving side.
+uint8_t led_pin = D0;
+uint8_t button_pin = D1;
+boolean button_state = 0;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(led_pin, OUTPUT);
+  pinMode(button_pin, INPUT);
+  radio.begin();                   //Starting the Wireless communication
+  radio.openWritingPipe(address);  //Setting the address where we will send the data
+  radio.setPALevel(RF24_PA_MIN);   //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
+  radio.stopListening();           //This sets the module as transmitter
+}
+void loop() {
+  button_state = digitalRead(button_pin);
+  if (button_state == HIGH) {
+    digitalWrite(led_pin, HIGH);
+    Serial.println("Button On");
+    const char text[] = "Your Button State is HIGH";
+    radio.write(&text, sizeof(text));  //Sending the message to receiver
+  } else {
+    digitalWrite(led_pin, LOW);
+    const char text[] = "Your Button State is LOW";
+    radio.write(&text, sizeof(text));  //Sending the message to receiver
+  }
+  radio.write(&button_state, sizeof(button_state));  //Sending the message to receiver
+  delay(1000);
+}
